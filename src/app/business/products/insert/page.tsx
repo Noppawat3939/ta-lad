16f0b969a0de9b8e8a810cb7f7ee938dafc5c4df
Gopgap <Noppawat3984@gmail.com>
2024-09-27 22:44:18 +0700

    create branch

---
+++ src/app/business/products/insert/page.tsx
@@ -0,0 +1,12 @@
+import { SidebarLayout } from "@/components";
+
+export default function InsertProductPage() {
+  return (
+    <SidebarLayout
+      classNames={{ contentLayout: "px-4 py-3" }}
+      activeSubMenuKey="insert"
+    >
+      page
+    </SidebarLayout>
+  );
+}
